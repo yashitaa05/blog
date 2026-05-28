7d5427d8d1c5c71fb9f58069c61e41cd205154b5
Yashita Kharbanda <yashitakharbanda2505@gmail.com>
2026-05-28 12:22:23 +0530

    new

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname,'/public')));
 app.use(express.json());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"*",
     methods:["POST","GET","DELETE","PUT"],
     credentials:true,
 }));
